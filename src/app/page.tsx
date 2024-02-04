3e7a04341d24596f56406136014d84e93354e395
Yurem1 <yuremtovar1@gmail.com>
2024-02-04 16:36:36 -0600

    give each component its own div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import React from 'react';
+import Converter from "@/components/converter";
+import Info from '@/components/info';
 
-export default function Home() {
+export default function Home(): React.JSX.Element {
   return (
-    <main>
-      
+    <main className="flex flex-col items-center justify-center h-screen bg-gray-50">
+      <div className='m-5'>
+        <Info/>
+      </div>
+      <div className=''>
+        <Converter/>
+      </div>
     </main>
   );
 }
