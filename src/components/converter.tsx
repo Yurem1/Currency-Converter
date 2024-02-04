bd19b80b26c18b0d8cff13f2a3136dde18a2bfd0
Yurem1 <yuremtovar1@gmail.com>
2024-02-04 16:38:04 -0600

    stack elements by column and assign gap

--- src/components/converter.tsx
+++ src/components/converter.tsx
@@ -4,7 +4,7 @@ import Options from './options';
 
 export default function Converter(): React.JSX.Element {
     return (
-        <div className='md flex flex-row'>
+        <div className='md flex flex-col gap-3.5'>
             <Input/>
             <Options/>
         </div>        
