719a05c3e48423a010e0390f11030db4340cc0c1
Yurem1 <yuremtovar1@gmail.com>
2024-02-04 23:22:37 -0600

    update imports, add refs for input, and output

--- src/components/converter.tsx
+++ src/components/converter.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+'use client';
+
+import React, { useRef } from 'react';
 import Input from './input';
 import Options from './options';
+import Output from './output';
+import Exchange from '@/utility/exchange';
 
 export default function Converter(): React.JSX.Element {
+    const inputRef = useRef<HTMLInputElement>();
+    const outputRef = useRef<HTMLInputElement>();
+    console.log(Exchange.findCurrency())
     return (
         <div className='md flex flex-col gap-3.5'>
             <Input/>
             <Options/>
+            <Output/>
         </div>        
-    )
+    );
 }
