c9696e5a793051bb9fb49390fa21f919e5ed137e
Yurem1 <yuremtovar1@gmail.com>
2024-02-06 00:35:10 -0600

    optimize and simplify input elements

--- src/components/options.tsx
+++ src/components/options.tsx
@@ -1,39 +1,29 @@
 'use client'
 
 import React from 'react';
+import CURRENCY_DATA from '../utility/currency.json';
 
-const Options = React.forwardRef<HTMLDivElement>(function Options(props, ref) {
+const Options = React.forwardRef<HTMLDivElement>(function Options(props, ref) { 
+
+    function displayOptions(): React.JSX.Element[] {
+        return Object.entries(CURRENCY_DATA).map(([key, data]) => {
+            return (
+                <div key={data.name}>
+                    <input type='radio' name='currency' value={data.value}/>
+                    <label htmlFor={data.name}>{key}</label>
+                </div>
+            );
+        });
+    }
+
     return (
         <div>
             <fieldset className='flex flex-row gap-3.5 items-center justify-center'>
                 <legend className='text-center	'>Choose Currency</legend>
-                <div>
-                    <input 
-                    type='radio' 
-                    id='FRANC' 
-                    value='FRF' 
-                    name='currency'/>
-                    <label htmlFor='FRANC'>FRF</label>
-                </div>
-                <div>
-                    <input 
-                    type='radio' 
-                    id='POUND' 
-                    value='GBP' 
-                    name='currency'/>
-                    <label htmlFor='POUND'>GBP</label>
-                </div>
-                <div>
-                    <input 
-                    type='radio' 
-                    id='EURO' 
-                    value='EUR' 
-                    name='currency'/>
-                    <label htmlFor='EURO'>EUR</label>
-                </div>
+                {displayOptions()}
             </fieldset>
         </div>
-    )
+    );
 });
 
 export default Options;
