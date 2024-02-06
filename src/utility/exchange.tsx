64d0024e705d3934ae2058a3de8165d1d7c21a63
Yurem1 <yuremtovar1@gmail.com>
2024-02-06 00:35:50 -0600

    add exchange function

--- src/utility/exchange.tsx
+++ src/utility/exchange.tsx
@@ -1,11 +1,24 @@
-import CURRENCIES from '../utility/currency.json'
+import CURRENCY_DATA from '../utility/currency.json'
 
+/**
+ * Represents a utility class for currency exchange.
+ */
 export default class Exchange {
-    static findCurrency() {
-        return JSON.stringify(CURRENCIES);
-    }
+    
+    /**
+     * Finds the value of a currency in the CURRENCIES object.
+     * @param currency - The currency to search for.
+     * @returns The value of the currency multiplied by 2 if found, otherwise 0.
+     */
+    public static exchange(currency: string): number {
+        const currencyEntries = Object.entries(CURRENCY_DATA);
 
-    static exchange() {
-        return 0;
+        for(let i = 0; i < currencyEntries.length; i++) {
+            if(currencyEntries[i][0] === currency) {
+                return currencyEntries[i][1].value * 2
+            }
+        }
+
+        return 0
     }
 }
