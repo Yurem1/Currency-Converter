aa0928caaeb2663f229929fadb14d12d9c42188c
Yurem1 <yuremtovar1@gmail.com>
2024-02-08 22:12:24 -0600

    add useReducer, handle input, output, and options dynamically

--- src/components/converter.tsx
+++ src/components/converter.tsx
@@ -1,20 +1,40 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React, { ChangeEvent } from 'react';
 import Input from './input';
 import Options from './options';
 import Output from './output';
-import Exchange from '@/utility/exchange';
+import Exchange, { IExchangeParams } from '@/utility/exchange';
 
 export default function Converter(): React.JSX.Element {
-    const inputRef = useRef<HTMLInputElement>();
-    const outputRef = useRef<HTMLInputElement>();
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    const optionsRef = React.useRef<HTMLInputElement>(null);
+
+    const [state, dispatch] = React.useReducer(
+        Exchange.reducer, { 
+            currency: '',
+            amount: 0
+        } as IExchangeParams
+    )
+
+
+    const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        dispatch(Number(event.target.value));
+    }
+
+    const optionsHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        dispatch(String(event.target.getAttribute('itemID')))
+    }
+
+    const outputValue: () => number = () => {
+        return Exchange.exchange(state)
+    }
 
     return (
         <div className='md flex flex-col gap-3.5'>
-            <Input/>
-            <Options/>
-            <Output/>
+            <Input ref={inputRef} change={inputHandler}/>
+            <Options ref={optionsRef} change={optionsHandler} />
+            <Output value={outputValue()}/>
         </div>        
     );
 }
