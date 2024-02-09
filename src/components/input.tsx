9597d66930ab9f642e3abff16d27666faf909293
Yurem1 <yuremtovar1@gmail.com>
2024-02-08 22:12:36 -0600

    add input parameters

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,18 +1,18 @@
-import React, { forwardRef } from 'react';
+import React, { ChangeEvent, forwardRef } from 'react';
 
-const Input = forwardRef<HTMLInputElement>(function Input(props, ref) {
-1
+interface IinputParams {
+    change?: (
+        event: ChangeEvent<HTMLInputElement>
+    ) => void;
+}
+
+const Input = forwardRef<HTMLInputElement, IinputParams>(function Input(props, ref) {
     return (
         <div className={`inline-block align-top`}>
-            <input 
-            ref={ref}
-            type='number' 
-            min={0}
-            placeholder='Enter Amount'
-            className='sm bg-gray-100 rounded-lg text-black border-slate-300 border-2 h-12 text-xl'
-            />
+            <input ref={ref} type='number' min={0} placeholder='Enter Amount' onChange={props.change}
+            className='sm bg-gray-100 rounded-lg text-black border-slate-300 border-2 h-12 text-xl'/>
         </div>
-    )
+    );
 });
 
 export default Input;
