381da18f1336f7f8977cd4588bb82d26318de926
Yurem1 <yuremtovar1@gmail.com>
2024-02-09 20:55:02 -0600

    improve exchange method, remove old comments, add new comments

--- src/utility/exchange.tsx
+++ src/utility/exchange.tsx
@@ -1,5 +1,8 @@
 import CURRENCY_DATA from '../utility/currency.json'
 
+/**
+ * Represents the parameters for a currency exchange.
+ */
 export interface IExchangeParams {
     currency: string;
     amount: number;
@@ -11,17 +14,17 @@ export interface IExchangeParams {
 export default class Exchange {
     /**
      * Finds the value of a currency in the CURRENCIES object.
-     * @param obj - The exchange parameters.
-     * @param obj.currency - The currency to search for.
-     * @param obj.amount - The amount of currency to exchange.
+     * @param obj - The object containing currency name, and amount to exchange
      * @returns The value of the currency multiplied by the amount if found, otherwise 0.
      */
     public static exchange(obj: IExchangeParams): number {
         const entries = Object.entries(CURRENCY_DATA);
 
-        for(let i = 0; i < entries.length; i++) {
-            if(entries[i][0] === obj.currency) {
-                return Number((entries[i][1].value * obj.amount).toFixed(3));
+        for(const [key, { value }] of entries) {
+            if(key === obj.currency) {
+                return Number(
+                    (obj.amount * value).toFixed(3)
+                )
             }
         }
 
