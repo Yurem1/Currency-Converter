8b59f2fb57c6f1d4dbea448b4f60aba698f2c11f
Yurem1 <yuremtovar1@gmail.com>
2024-02-11 23:00:54 -0600

    Fix typo in header

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 export default function Info(): React.JSX.Element {
     return (
         <div className='text-center'>
-            <h1 className=''>Currency Converter/</h1>
+            <h1 className=''>Currency Converter</h1>
             <p>Type in how much you wanna convert in dollars</p>
         </div>
     );
